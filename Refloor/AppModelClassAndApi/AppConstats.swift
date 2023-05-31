d6f6243f0cff01ed224ac8349764aaea960ef560
Bincy <bincy.aliyar@oneteamus.com>
2023-05-31 13:42:47 +0530

    production url update

--- Refloor/AppModelClassAndApi/AppConstats.swift
+++ Refloor/AppModelClassAndApi/AppConstats.swift
@@ -22,9 +22,9 @@ struct AppURL {
      //let BASE_URL = "http://server.oneteamus.com:2445/api/"
     
     //Offline live//7007  - new server
-     let LIVE_BASE_URL = "http://server.oneteamus.com:2446/api/"
+     //let LIVE_BASE_URL = "http://server.oneteamus.com:2446/api/"
      //let LIVE_BASE_URL = "http://35.199.10.7:7007/api/"
-   // let LIVE_BASE_URL = "http://odoo.myx.ac:7007/api/"
+    let LIVE_BASE_URL = "http://odoo.myx.ac:7007/api/"
 //http://server.oneteamus.com:2446
     //LIVE
     // let BASE_URL = "http://35.245.254.221:7007/api/"
