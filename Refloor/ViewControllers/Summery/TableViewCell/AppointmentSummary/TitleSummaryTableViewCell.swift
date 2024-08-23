ca15aa09e8bf0919f75485924b558959c274e6d9
ajayjayaram <33627068+ajayjayaram@users.noreply.github.com>
2024-08-23 16:16:00 +0530

    Summary Page UI implementation summary page functionality implementation

---
+++ Refloor/ViewControllers/Summery/TableViewCell/AppointmentSummary/TitleSummaryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TitleSummaryTableViewCell.swift
+//  Refloor
+//
+//  Created by Bincy C A on 12/08/24.
+//  Copyright © 2024 oneteamus. All rights reserved.
+//
+
+import UIKit
+
+class TitleSummaryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
