e128937d8113d7b19ad4e46c8d1fc69bf4a3168e
ajayjayaram <33627068+ajayjayaram@users.noreply.github.com>
2024-12-05 18:30:56 +0530

    delivery options implementation in measurement list view controller

---
+++ Refloor/ViewControllers/Summery/TableViewCell/AppointmentPaymentSummaryViewController.swift
@@ -0,0 +1,61 @@
+//
+//  AppointmentPaymentSummaryViewController.swift
+//  Refloor
+//
+//  Created by Bincy C A on 12/08/24.
+//  Copyright © 2024 oneteamus. All rights reserved.
+//
+
+import UIKit
+
+class AppointmentPaymentSummaryViewController: UIViewController {
+    
+    static func initialization() -> AppointmentPaymentSummaryViewController? {
+        return UIStoryboard(name:"Main", bundle: nil).instantiateViewController(withIdentifier: "AppointmentPaymentSummaryViewController") as? AppointmentPaymentSummaryViewController
+    }
+    
+    
+    var totalPrice:Double = Double()
+    var finalPayment:Double = Double()
+    var financeAmount:Double = Double()
+    var downPayment:Double = 0
+    var isOutOfstock = false
+
+    @IBOutlet weak var paymentView: UIView!
+    @IBOutlet weak var outOfStockView: UIView!
+    @IBOutlet weak var downPaymentLbl: UILabel!
+    @IBOutlet weak var financeAmountLbl: UILabel!
+    @IBOutlet weak var finalPaymentLbl: UILabel!
+    @IBOutlet weak var totalPriceLblSummary: UILabel!
+    @IBOutlet weak var totalPriceLbl: UILabel!
+    override func viewDidLoad() 
+    {
+        super.viewDidLoad()
+        if isOutOfstock
+        {
+            outOfStockView.isHidden = false
+            paymentView.isHidden = true
+        }
+        else
+        {
+            outOfStockView.isHidden = true
+            paymentView.isHidden = false
+        }
+        
+        downPaymentLbl.text = "$\(downPayment.toDoubleString)"
+        finalPaymentLbl.text = "$\(finalPayment.toDoubleString)"
+        financeAmountLbl.text = "$\(financeAmount.toDoubleString)"
+        totalPriceLbl.text = "$\(totalPrice.toDoubleString)"
+        totalPriceLblSummary.text = "$\(totalPrice.toDoubleString)"
+
+       
+    }
+    
+
+    @IBAction func okBtnClicked(_ sender: UIButton) 
+    {
+        
+        self.dismiss(animated: true)
+    }
+
+}
